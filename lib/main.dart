f269e7b942a778bf9e1acb10b2b208e0081359e8
raju-islam <rajuslam39@gmail.com>
2024-01-24 22:17:33 +0600

    stepper widget add

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled15/stepper_widget.dart';
 
 import 'date_picker.dart';
 
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  MyHomePage(),
+      home:  StepperWidget(),
     );
   }
 }
